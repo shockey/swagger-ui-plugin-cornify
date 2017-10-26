730e670a9fdca5c5f7e9d1cade6688f712833b2f
Kyle Shockey <shockey@users.noreply.github.com>
2017-10-25 19:32:49 -0700

    Init

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import cornify from "cornified"
+
+let intervalId = null
+
+function updateJsonSpec(originalAction) {
+  return (...args) => {
+    let [spec] = args
+    originalAction(...args)
+
+    // spec is a JS object
+    if(spec && spec["x-cornify"] && spec["x-cornify"] === true) {
+      if(intervalId === null) {
+        intervalId = setInterval(() => {
+          cornify.add()
+        }, 1000)
+      }
+    } else {
+      cornify.clear()
+      clearInterval(intervalId)
+      intervalId = null
+    }
+  }
+}
+
+export default function() {
+  return {
+    statePlugins: {
+      spec: {
+        wrapActions: {
+          updateJsonSpec
+        }
+      }
+    }
+  }
+}
